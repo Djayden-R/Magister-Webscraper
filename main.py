3379b5757bc0382a0f80aa5eba76da1fd5523c38
Djayden-R <6953@rgomiddelharnis.nl>
2026-03-06 22:31:25 +0100

    add logic to check token and print calendar

--- main.py
+++ main.py
@@ -2,46 +2,82 @@ import json
 import logging
 from fetch_magister import fetch_magister_calendar, fetch_magister_token
 from pathlib import Path
+import datetime
 
 PROGRAM_PATH = Path("/usr/src/app")
 OPTIONS_FILE_PATH = "/data/options.json"
 
-def get_credentials():
+
+def get_options():
     with open(OPTIONS_FILE_PATH, 'r') as f:
         options = json.load(f)
 
     # Load dictionary with credentials
     # Expected format: [{'username': username, 'password': password}, ...]
     credentials_list: list[dict] = options['credentials']
+    days_to_fetch: int = options['days_to_fetch']
 
-    return credentials_list
+    return credentials_list, days_to_fetch
 
-def get_tokens():
+
+def get_user_info(username):
     token_path = PROGRAM_PATH / "tokens.json"
+
+    token = user_id = None
 
     if token_path.exists():
         with open(token_path, 'r') as f:
-            pass
-    else:
-        open(token_path, 'x') # Create tokens file
+            data = json.load(f)
+
+        user_info = data.get(username, None)
+
+        if user_info:
+            token = user_info.get("token", None)
+            user_id = user_info.get("user_id", None)
+            
+    return token, user_id
 
 
 def main():
-    credentials_list = get_credentials()
+    credentials_list, days_to_fetch = get_options()
+
+    if not credentials_list:
+        logging.warning("Credentials not defined, exiting program...")
+        return
 
     for credenials in credentials_list:
         username = credenials.get('username', None)
         password = credenials.get('password', None)
 
-        if not(username and password):
+        if not (username and password):
             logging.error(f"Invalid credentials found (username={username}, password={password})")
             continue
 
+        token, user_id = get_user_info(username)
+
+        if token and user_id:
+            calendar = fetch_magister_calendar(user_id, token, days_to_fetch)
+    
+        if not (token and user_id) or not calendar:
+            user_id, token = fetch_magister_token(username, password)
+        
+        calendar = fetch_magister_calendar(user_id, token, days_to_fetch)
 
 
 
+        lessons = calendar['Items']
+        prev_day = None
 
-    if credentials_list:
-        pass
-    else:
-        logging.warning("Credentials not defined, exiting program...")
+        for lesson in lessons:
+            date_str = lesson["Start"]
+            date = datetime.fromisoformat(date_str.replace("Z", "+00:00"))
+            day = date.strftime("%A (%d/%m)")
+
+            if day != prev_day:
+                logging.info(day)
+                prev_day = day
+
+            logging.info(f"{lesson['LesuurVan']}e hour - {lesson['Omschrijving']} {lesson['Lokatie']}")
+
+if __name__ == "__main__":
+    main()
